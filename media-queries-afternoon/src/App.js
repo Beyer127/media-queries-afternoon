9c9c9c7b3c391c004e90468a76cc52248cb933cf
Beyer127 <67298642+Beyer127@users.noreply.github.com>
2020-07-29 17:01:23 -0700

    media-queries-afternoon-project

---
+++ media-queries-afternoon/src/App.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <div id="nabar">
+      <img className="nav-logo" src="https://startbootstrap.github.io/startbootstrap-agency/assets/img/navbar-logo.svg"/>    
+        <nav className="navbar">
+          <button>SERVICES</button>
+          <button>PORTFOLIO</button>
+          <button>ABOUT</button>
+          <button>CONTACT</button>
+        </nav>
+      </div>
+    </div>
+  );
+}
+
+export default App;
